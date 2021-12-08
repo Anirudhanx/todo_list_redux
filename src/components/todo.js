1aadde2be58b2b809557884d464cbd8b6c5ee007
Anirudhan <42598004+Anirudhanx@users.noreply.github.com>
2021-12-08 17:37:30 +0530

    Update todo.js

--- src/components/todo.js
+++ src/components/todo.js
@@ -50,7 +50,8 @@ const Todo = () => {
           </div>
 
           <div className="showItems">
-            <button className="btn effect04" data-sm-link-text="Remove All">
+            <button className="btn effect04" data-sm-link-text="Remove All" 
+            onClick={() => dispatch(removeTodo())}>
               <span>
                 Check List
               </span>
